a02f9afe47bc9e2fa60fbbdcc565886cbcd868c0
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-07-04 16:57:16 -0400

    Add new loading message for Heroku

--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
+import axios from 'axios'
+import loadingB from '../assets/loadingB.svg'
 
-import Podcasts from '../components/Podcasts';
+import Podcasts from '../components/Podcasts'
 
 function HomeSection() {
-  const [podcasts, setPodcasts] = useState([]);
+  const [podcasts, setPodcasts] = useState([])
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_SERVER_URL}/podcasts`)
@@ -19,7 +20,14 @@ function HomeSection() {
   return (
     <section className='justify-self-center gap-12 w-8/12'>
       <h1 className='font-bold text-3xl mb-12 mt-6'>Top Podcasts</h1>
-      <Podcasts podcasts={podcasts} />
+      {podcasts.length === 0 ?
+        <div className='font-bold text-lg mt-28'>
+          <img src={loadingB} alt="loading animation" className='mx-auto mt-64' />
+          Please allow 15 ~ 20 secconds for Heroku server to start up. If no contents showing after 20 seconds, refresh the page to load them up.
+        </div>
+        :
+        <Podcasts podcasts={podcasts} />
+      }
       <div className='h-36'></div>
     </section>
   )
